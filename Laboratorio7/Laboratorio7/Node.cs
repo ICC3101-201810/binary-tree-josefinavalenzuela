1c7d1710967ff917170affc3d43366ed372168a0
macanepa <macanepa@miuandes.cl>
2018-04-24 16:13:25 -0300

    First commit for lab 7

---
+++ Laboratorio7/Laboratorio7/Node.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7
+{
+    public class ArbolBinario<T>
+    {
+        public class Nodo<T>
+        {
+            public T value;
+            public Nodo<T>[] nodos = new Nodo<T>[2];
+            public Nodo<T> HijoDer;
+            public Nodo<T> HijoIzq;
+
+        }
+        Nodo<T> raiz;
+        public ArbolBinario()
+        {
+            raiz = null;
+        }
+
+        public void Insertar(T obj, Nodo<T> Padre, int donde)
+        {
+            //donde: 0 izq, 1 der
+            Nodo<T> n = new Nodo<T>();
+            n.HijoIzq = null;
+            n.HijoDer = null;
+            n.value = obj;
+            n.nodos[1] = n.HijoIzq;
+            n.nodos[2] = n.HijoDer;
+            if (Padre.nodos[donde] == null)
+            {
+                Padre.nodos[donde] = n;
+            }
+            else if (donde == 0 && Padre == null)
+            {
+                raiz = n;
+            }
+            else
+            {
+                Console.WriteLine("El nodo se encuentra ocupado");
+            }
+            
+        }
+    }
+}
---
+++ Laboratorio7/Laboratorio7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 0;
+
+            while (w == 0)
+            {
+                Console.WriteLine("Ingrese el tipo de su arbol generico");
+                string tipo = Console.ReadLine();
+
+
+                Console.WriteLine("Ingrese el obj de su nodo");
+                string obj = Console.ReadLine();
+                Console.WriteLine("Ingrese el padre de su nodo");
+                string padre = Console.ReadLine();
+                Console.WriteLine("Ingrese si lo quiere a la izquierda (0) o derecha (1) de este");
+                int posicion = Convert.ToInt32(Console.ReadLine());
+
+                ArbolBinario<string> t = new ArbolBinario<string>();
+                t.Insertar(obj, padre, posicion);
+
+
+            }
+        }
+    }
+}
